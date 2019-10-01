ee9ce3a4c838244fd761b18fa87094363815292c
Michael Herda Setiawan <michaelherda@mail.unpas.ac.id>
2019-10-01 22:23:09 +0700

    Menambahan method

--- produk.php
+++ produk.php
@@ -10,10 +10,26 @@
  			$penerbit,
  			$harga = 0;
 
+ 	public function sayHello()  {
+ 		return "hello world";
+ 	}
+
  }
 
- $produk1 = new Produk();
- $produk1->judul="Naruto";
+ // $produk1 = new Produk();
+ // $produk1->judul="Naruto";
 
- $produk2 = new Produk();
- $produk2->judul = "Uncharted";
+ // $produk2 = new Produk();
+ // $produk2->judul = "Uncharted";
+
+ $produk3 = new Produk();
+ $produk3->judul = "Naruto";
+ $produk3->penulis = "Masashi Kishimoto ";
+ $produk3->penerbit = "Shonen Jump";
+ $produk3->harga = "30000";
+
+ echo "Komik :  $produk3->penulis, $produk3->penerbit";
+ echo "<br>";
+ echo $produk3->sayHello();
+
+ 
