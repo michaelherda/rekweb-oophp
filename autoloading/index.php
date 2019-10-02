d3b5f5f81c5521716451e3ccb73760b27b4399e4
Michael Herda Setiawan <michaelherda@mail.unpas.ac.id>
2019-10-02 23:57:23 +0700

    Menambah spl autoload register pada init.php

--- autoloading/index.php
+++ autoloading/index.php
@@ -1,5 +1,7 @@
 <?php 
 
+require_once 'App/init.php';
+
 $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
  $produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 50);
 
