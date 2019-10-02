2aa61ce8cf87758d87403a90a339ca77bede3ae5
Michael Herda Setiawan <michaelherda@mail.unpas.ac.id>
2019-10-02 20:50:23 +0700

    menambahkan class didalam class constant.php

--- constant.php
+++ constant.php
@@ -1,11 +1,23 @@
 <?php 
 
-define('NAMA', 'Michael Herda');
-echo NAMA;
+// define('NAMA', 'Michael Herda');
+// echo NAMA;
 
-echo "<br>";
+// echo "<br>";
 
-const UMUR = 17;
-echo UMUR;
+// const UMUR = 17;
+// echo UMUR;
 
- ?>
+/**
+ * 
+ */
+class Coba
+{
+	const NAMA = 'Michael';
+	function __construct(argument)
+	{
+		# code...
+	}
+}
+
+echo Coba::NAMA;
