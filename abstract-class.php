1d689679b58ca314819fb10e69c93fece1076fac
Michael Herda Setiawan <michaelherda@mail.unpas.ac.id>
2019-10-02 21:37:14 +0700

    menambahkan getInfo sebagai pengganti dari getInfoProduk

--- abstract-class.php
+++ abstract-class.php
@@ -67,10 +67,14 @@
  		return "$this->penulis, $this->penerbit";
  	}
 
- 	public function getInfoProduk(){
+ 	abstract public function getInfoProduk();
+ 		
+ 	publict function getInfo(){
  		$str = "{$this->judul}|{$this->getLabel()}(Rp. {$this->harga})";
  	
  		return $str;
+ 	}
+
  	}
 
  } 
@@ -139,13 +143,13 @@
 
 
 
- // $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
- // $produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 50);
+ $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+ $produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 50);
 
- // $cetakProduk = new CetakInfoProduk();
- // $cetakProduk->tambahProduk($produk1);
- // $cetakProduk->tambahProduk($produk2);
- // echo $cetakProduk->cetak();
+ $cetakProduk = new CetakInfoProduk();
+ $cetakProduk->tambahProduk($produk1);
+ $cetakProduk->tambahProduk($produk2);
+ echo $cetakProduk->cetak();
 
 
  
